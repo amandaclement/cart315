319be57905e9fd54f8cdd3b6061c2ff3aefade16
Amanda Clement <amanda.clement22@hotmail.com>
2021-02-03 10:11:43 -0500

    Exercise 2: Update

--- exercise2/Assets/pickup.cs
+++ exercise2/Assets/pickup.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class pickup : MonoBehaviour
 {
-    public string tag;
+    public string pickuptag;
+    //public string tag;
 
     private void OnCollisionEnter(Collision collision)
     {
-
-        if (collision.collider.gameObject.tag == "pickup")
+        if (collision.collider.gameObject.CompareTag(pickuptag))
+        // if (collision.collider.gameObject.tag == "pickup")
         {
+            Debug.Log("collision detected wih pickup");
             Destroy(collision.collider.gameObject);
         }
     }
