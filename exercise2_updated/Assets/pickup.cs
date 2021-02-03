11dbb45a6569afea1ae18044f8779ab96b8fa007
Amanda Clement <amanda.clement22@hotmail.com>
2021-02-03 12:35:32 -0500

    Exercise 2: Playing with physics/materials

--- exercise2_updated/Assets/pickup.cs
+++ exercise2_updated/Assets/pickup.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// need to fix this file
 
 public class pickup : MonoBehaviour
 {
-    public string pickuptag;
-    //public string tag;
+    //public string pickuptag;
+    public string tag;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.gameObject.CompareTag(pickuptag))
-        // if (collision.collider.gameObject.tag == "pickup")
+        Debug.Log("initial collide");
+        //if (collision.collider.gameObject.CompareTag(pickuptag))
+        if (collision.collider.gameObject.tag == "pickup")
         {
             Debug.Log("collision detected wih pickup");
             Destroy(collision.collider.gameObject);
+            Destroy(gameObject);
         }
     }
 
