8e14a6e65b819e8066abc7cba12323e47296db0a
Amanda Clement <amanda.clement22@hotmail.com>
2021-02-05 23:00:58 -0500

    Prototype: Updated player behaviour, plane tilt (targetting), and camera tracking

--- prototype1/Assets/Player.cs
+++ prototype1/Assets/Player.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // reference (character controller tutorial): https://www.youtube.com/watch?v=59No0ybIoxg
-// reference for rotating behaviour: https://answers.unity.com/questions/1202034/smooth-90-degree-rotation-on-keypress.html
 
 public class Player : MonoBehaviour
 {
     [SerializeField] // used so that variable cannot be changed by other scripts
     private float moveSpeed = 5f;
-    [SerializeField] 
+    [SerializeField]
     private float gravity = 9.81f;
-    [SerializeField] 
+    [SerializeField]
     private float jumpSpeed = 3.5f;
     [SerializeField]
     private float doubleJumpMultiplier = 0.5f;
@@ -22,8 +21,27 @@ public class Player : MonoBehaviour
 
     private bool canDoubleJump = false;
 
-    // needed to control flipping of plane (on double jump)
-    public GameObject Plane;
+    // reference: https://docs.unity3d.com/ScriptReference/GameObject.FindGameObjectsWithTag.html?_ga=2.125981838.2057613814.1612463807-1308570294.1603245419
+    // detecting which plane the player is closest to figure out which one to flip (on double jump)
+    public GameObject FindClosestPlane()
+    {
+        GameObject[] planes;
+        planes = GameObject.FindGameObjectsWithTag("Plane");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject plane in planes)
+        {
+            Vector3 diff = plane.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = plane;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
 
 
     // Start is called before the first frame update
@@ -32,6 +50,7 @@ public class Player : MonoBehaviour
         controller = GetComponent<CharacterController>();
     }
 
+    // reference: https://answers.unity.com/questions/1202034/smooth-90-degree-rotation-on-keypress.html
     // handling the rotation (flip) of the plane
     IEnumerator RotatePlane(Vector3 byAngles, float inTime)
     {
@@ -39,7 +58,7 @@ public class Player : MonoBehaviour
         var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
         for (var t = 0f; t < 1; t += Time.deltaTime / inTime)
         {
-            Plane.transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            FindClosestPlane().transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
             yield return null;
         }
     }
@@ -61,9 +80,10 @@ public class Player : MonoBehaviour
                 directionY = jumpSpeed;
             }
         }
-        else { 
-        if (Input.GetButtonDown("Jump") && canDoubleJump) // for some reason this only really works with GetButtonDown
+        else
         {
+            if (Input.GetButtonDown("Jump") && canDoubleJump) // for some reason this only really works with GetButtonDown
+            {
                 Debug.Log("double jump");
                 directionY = jumpSpeed * doubleJumpMultiplier;
                 canDoubleJump = false;
@@ -75,6 +95,6 @@ public class Player : MonoBehaviour
 
         directionY -= gravity * Time.deltaTime;
         direction.y = directionY;
-        controller.Move(direction * moveSpeed * Time.deltaTime);  
+        controller.Move(direction * moveSpeed * Time.deltaTime);
     }
 }
--- prototype1/Assets/Tilt.cs
+++ prototype1/Assets/Tilt.cs
@@ -7,22 +7,24 @@ using UnityEngine;
 public class Tilt : MonoBehaviour
 {
     public Transform player;
+    public GameObject target;
+
     public float sensitivity = 3f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 diff = (player.position - transform.position).normalized;
-        diff *= sensitivity;
-        diff.y = 1;
-        Quaternion target = Quaternion.FromToRotation(transform.up, diff);
-        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime);
-
+            Vector3 diff = (player.position - transform.position).normalized;
+            diff *= sensitivity;
+            diff.y = 1;
+            Quaternion target = Quaternion.FromToRotation(transform.up, diff);
+            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime);
     }
 }
